ecc284c465f18957b3e22cc5951c1cfbafbd2a15
thiwashwe <138187434+thiwashwe@users.noreply.github.com>
2023-08-23 23:24:53 +0630

    First Commit
    First Date 23-8-2023

---
+++ assets/js/login2.js
@@ -0,0 +1,73 @@
+// const formOpenBtn = document.querySelector("#form-open"),
+//   home = document.querySelector(".home"),
+//   formContainer = document.querySelector(".form_container"),
+//   formCloseBtn = document.querySelector(".form_close"),
+//   signupBtn = document.querySelector("#signup"),
+//   loginBtn = document.querySelector("#login"),
+//   pwShowHide = document.querySelectorAll(".pw_hide");
+
+// formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+// formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+// pwShowHide.forEach((icon) => {
+//   icon.addEventListener("click", () => {
+//     let getPwInput = icon.parentElement.querySelector("input");
+//     if (getPwInput.type === "password") {
+//       getPwInput.type = "text";
+//       icon.classList.replace("uil-eye-slash", "uil-eye");
+//     } else {
+//       getPwInput.type = "password";
+//       icon.classList.replace("uil-eye", "uil-eye-slash");
+//     }
+//   });
+// });
+
+// signupBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   formContainer.classList.add("active");
+// });
+// loginBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   formContainer.classList.remove("active");
+// });
+
+// navbar
+
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", () => {
+  navbar.classList.toggle("showInput");
+  if (navbar.classList.contains("showInput")) {
+    searchBox.classList.replace("bx-search", "bx-x");
+  } else {
+    searchBox.classList.replace("bx-x", "bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+};
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+};
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+};
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function () {
+  navLinks.classList.toggle("show2");
+};
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+  navLinks.classList.toggle("show3");
+};
